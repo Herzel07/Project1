cebb4952f227be50884792f526088c7b4dbd1f1c
Herzel07 <herzeldandile@gmail.com>
2023-10-31 22:06:33 +0200

    Notes

---
+++ Notes/script.js
@@ -0,0 +1,6 @@
+var container2 = document.getElementsByClassName
+(container2)(0);
+var container3 = document.getElementsByClassName
+(container3)(0);
+var checkIcon = document.getElementById
+var xicon = document.getElementById
