bf9ebcf8c88296fd41dee6f9ba25b52df182eb3e
Herzel07 <herzeldandile@gmail.com>
2023-11-06 20:08:43 +0200

    Notes

--- Notes/script.js
+++ Notes/script.js
@@ -2,5 +2,41 @@ var container2 = document.getElementsByClassName
 (container2)(0);
 var container3 = document.getElementsByClassName
 (container3)(0);
-var checkIcon = document.getElementById
-var xicon = document.getElementById
+var checkIcon = document.getElementById("check-icon");
+var xIcon = document.getElementById("x-icon");
+var i = 0;
+xIcon.addEventListener("click", function(){
+    typeNote();
+})
+
+
+checkIcon.addEventListener("click", function(){
+    createNote();
+})
+
+function typeNote(){
+
+}
+function createNote(){
+
+}
+
+function margin(){
+var random_margin = ["5px", "1px", "5px", "10px", "15px", "20px"]
+
+return random_margin[Math.floor(math.random()*
+random_margin.length)];
+}
+
+function rotate(){
+
+    var random_rotate = ["rotate(3deg)" ]
+
+    return random_margin[Math.floor(math.random()*
+    random_margin.length)];
+
+}
+
+function color(){
+
+}
