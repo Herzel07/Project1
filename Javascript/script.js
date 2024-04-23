7a77db36895085777ccc3658ec92c469f3c1ba94
Herzel07 <herzeldandile@gmail.com>
2024-04-23 22:39:05 +0200

    JScript

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,9 @@
-consta
-let xp;
-let dev = "Hazel";
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
